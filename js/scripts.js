e3e3d073ea108601ee09ac608efa61fa428130f6
Kyle Parsons <23gdsm@gmail.com>
2018-06-01 16:39:53 -0700

    simplify j script to attempt fix, will not work

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,26 @@ $(document).ready(function() {
   $("#submit").click(function(event) {
     event.preventDefault();
     var system = parseInt($("#system").val());
-    var work = parseInt($("#work").val());
-    var project = parseInt($("#project").val());
-
-    var result = system + work + project;
-      if (result <= 6) {
+      if (system = 1) {
         $('#c').show();
-}        else if (result <=9) {
-          $('#php').show();
-}           else {
-          $('#project').show();
-}
+      } else if (system = 2) {
+        $('#php').show();
+      }else {
+        $('#ruby').show();
+      }
 
 
 
 
 
 
+//     var result = system + work + project;
+//       if (result <= 6) {
+//         $('#c').show();
+// }        else if (result <= 9) {
+//           $('#php').show();
+// }           else {
+//           $('#project').show();
 
   });
+});
